c7ad7cba81f0ecbfce9e0f5aa278d3f491abbcf4
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-16 23:49:49 +0530

    Update cicd.groovy1121

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,8 @@ def deploy(jobName,idAddress,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${idAddress}:/var/lib/tomcat9/webapps/${context}.war"
 }
+
+def deploy(jobName)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar'
+}
